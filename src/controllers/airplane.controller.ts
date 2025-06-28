a8c1859da1c1ba5f6ba21d441a2f301677102f75
waleed21121 <wa581509@gmail.com>
2025-06-28 18:08:38 +0300

    Fixed some errors

--- src/controllers/airplane.controller.ts
+++ src/controllers/airplane.controller.ts
@@ -1,30 +1,26 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { IAirplane } from "../schemas/airplane.schema";
 import { AirplaneService } from "../services";
 import { IAirplaneResponse } from "../DTOs";
 import { StatusCodes } from "http-status-codes";
 
-async function createAirplane (req: Request<{}, {}, IAirplane, {}, {}>, res: Response<IAirplaneResponse, {}>) {
+async function createAirplane (req: Request<{}, {}, IAirplane, {}, {}>, res: Response<IAirplaneResponse, {}>, next: NextFunction) {
     // Error handling coming soon ...
     try {
         const airplane = await AirplaneService.createAirplane(req.body);
-        return res
-                .status(StatusCodes.CREATED)
-                .send({
-                    success: true,
-                    message: 'Successfully created an airplane',
-                    data: airplane,
-                    error: null
-                })
+        res.status(StatusCodes.CREATED).send({
+            success: true,
+            message: 'Successfully created an airplane',
+            data: airplane,
+            error: null
+        })
     } catch (error: any) {
-        return res
-                .status(StatusCodes.CREATED)
-                .send({
-                    success: true,
-                    message: 'Successfully created an airplane',
-                    data: null,
-                    error: error
-                })
+        res.status(StatusCodes.CREATED).send({
+            success: true,
+            message: 'Something went wrong creating an airplane',
+            data: null,
+            error: error
+        })
     }
 }
 
