b05b2d1889445aa2f04c2d25af878eb7bd89b547
waleed21121 <wa581509@gmail.com>
2025-06-30 23:35:49 +0300

    Updated the attributes of the airplane model.

--- src/models/airplane.ts
+++ src/models/airplane.ts
@@ -1,16 +1,28 @@
 import { DataTypes, Model, Optional, Sequelize } from 'sequelize';
 
 export interface AirplaneAttributes {
+  id: number;
   modelNumber: string;
   capacity: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export class Airplane extends Model<AirplaneAttributes, Partial<AirplaneAttributes>> implements AirplaneAttributes {
+    id!: number
     modelNumber!: string;
     capacity!: number;
+    createdAt!: Date;
+    updatedAt!: Date;
     
     static initModel(sequelize: Sequelize): typeof Airplane {
       return Airplane.init({
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+        },
         modelNumber: {
           type: DataTypes.STRING,
           allowNull: false,
@@ -25,6 +37,14 @@ export class Airplane extends Model<AirplaneAttributes, Partial<AirplaneAttribut
             min: 1,
             max: 1500
           }
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
         }
       }, {
         sequelize,
