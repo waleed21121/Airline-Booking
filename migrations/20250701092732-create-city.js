80e7f6c1e37749625bba444942eecfe9c90491fa
waleed21121 <wa581509@gmail.com>
2025-07-01 12:48:52 +0300

    Fixed errors in migration.

--- migrations/20250701092732-create-city.js
+++ migrations/20250701092732-create-city.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
         unique: true
       },
