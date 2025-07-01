4d0a8c8b1d22fcb330bdcb6fc2bcd3d1611117d5
waleed21121 <wa581509@gmail.com>
2025-07-01 13:33:50 +0300

    Created city validators.

---
+++ src/validators/city.validators.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { validationMiddleware } from "../middlewares";
+import { CitySchema } from "../schemas/city.schema";
+import { IdSchema } from "../schemas/id.schema";
+import { UpdateCitySchema } from "../schemas/updateCity.schema";
+
+export const postCityValidator = validationMiddleware(z.object({}), CitySchema, z.object({}))
+export type TPostCity = typeof postCityValidator
+
+export const updateCityValidtor = validationMiddleware(IdSchema, UpdateCitySchema, z.object({}))
+export type TUpdateCity = typeof updateCityValidtor
