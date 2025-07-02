aad83e05a2e9f0f01c44ce03977dfe17f320b851
waleed21121 <wa581509@gmail.com>
2025-07-02 16:32:44 +0300

    Enhanced error message.

--- src/schemas/airport/airport.schema.ts
+++ src/schemas/airport/airport.schema.ts
@@ -4,7 +4,7 @@ import { uppercaseCheck } from '../../utils';
 
 export const AirportSchema = z.object({
     name: z.string({message: "The airport name must be string."}).min(5, "The airport name must be at least five caharacters."),
-    code: z.string({message: "The airport code must be string."}).length(3, "The airport code must consist of 3 characters.").refine(uppercaseCheck),
+    code: z.string({message: "The airport code must be string."}).length(3, "The airport code must consist of 3 characters.").refine(uppercaseCheck, "The airport code must be 3 uppercase characters."),
     address: z.string({message: "The airport address must be string."}).optional(),
     cityID: z.number({message: "The airport address must be number."})
 });
