23cb8354613f09b8314f68813f822ed5020ae8a8
waleed21121 <wa581509@gmail.com>
2025-07-03 18:28:35 +0300

    Created flight validators middleware.

--- src/validators/flight.validators.ts
+++ src/validators/flight.validators.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 import { validationMiddleware } from "../middlewares";
 import { FlightSchema } from "../schemas/flight/flight.schema";
 import { IdSchema } from "../schemas/id.schema";
+import { FlightQuerySchema } from "../schemas/query/flightQuery.schema";
 
 export const postFlightValidator = validationMiddleware(z.object({}), FlightSchema, z.object({}))
 export type TPostFlight = typeof postFlightValidator
+
+export const getFlightsValidator = validationMiddleware(z.object({}), z.object({}), FlightQuerySchema)
+export type TGetFlights = typeof getFlightsValidator
