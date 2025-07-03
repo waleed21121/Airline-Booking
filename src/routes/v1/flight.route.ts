38287cd6b980f1d3a1f29797ef8e16731103c8f8
waleed21121 <wa581509@gmail.com>
2025-07-03 18:30:15 +0300

    Added get flights route.

--- src/routes/v1/flight.route.ts
+++ src/routes/v1/flight.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { FlightController } from "../../controllers";
 import { wrapErrorMiddleware } from "../../middlewares";
 import { idValidator } from "../../validators/id.validator";
-import { postFlightValidator } from "../../validators/flight.validators";
+import { getFlightsValidator, postFlightValidator } from "../../validators/flight.validators";
 
 const flightRouter = Router();
 
 flightRouter.route('/')
                 .post(postFlightValidator, wrapErrorMiddleware(FlightController.createFlight))
+                .get(getFlightsValidator, wrapErrorMiddleware(FlightController.findFlights));
 
 export default flightRouter;
